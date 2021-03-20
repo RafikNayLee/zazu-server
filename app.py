4468b41c69a3130891252bb551da7399f34c8e97
RafikNayLee <rfknaili@gmail.com>
2021-03-20 20:34:49 +0100

    added Procfile

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from randomColor import getRandomColor
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///news-org.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 CORS(app)
 db = SQLAlchemy(app)
